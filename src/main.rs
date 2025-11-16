d78cee698899a73fc85099e57c05e30c0c3fdb51
Jeremy <Jeremy.Daug@gmail.com>
2025-11-15 22:22:39 -0600

    Running Fix real quick

--- src/main.rs
+++ src/main.rs
@@ -10,14 +10,17 @@ pub mod bang;
 use std::{env, io::{stdin, stdout}, time::{Duration, Instant}};
 
 use crossterm::{ExecutableCommand, style::Print};
-use macroquad::prelude::*;
 use ::rand as stdrng;
 
-use crate::{
-    coin_game::{coin_toss::CoinToss, coin_toss_cmd::select_screen}, common_state::{ButtonAction, CommonState}, gfx::coin::Coin, machine::machine::Machine, main_menu::main_menu};
+use bevy::prelude::*;
 
-#[macroquad::main("Improbability Machine")]
-async fn main() {
+use crate::{coin_game::{coin_toss::CoinToss, coin_toss_cmd::select_screen}, common_state::{ButtonAction, CommonState}, gfx::coin::Coin, machine::machine::Machine, main_menu::main_menu};
+
+fn main() {
+    App::new().run();
+}
+
+fn _old_main() {
     let args: Vec<String> = env::args().collect();
     let mode = &args[1];
     let start_time = Instant::now();
@@ -43,48 +46,7 @@ async fn main() {
         main_menu(&mut common_state);
     } else if mode == "ui" {
         let mut change = 0.0;
-
-        // load coin textures
-        let coin = Coin::load_coin().await;
-        build_textures_atlas();
-
-        let i = 0;
-        loop {
-            clear_background(DARKGRAY);
-            //draw_line(40.0, 40.0, 100.0, 200.0, 15.0, BLUE);
-            //draw_rectangle(screen_width() / 2.0 - 60.0, 100.0, 120.0, 60.0, GREEN);
-
-            let hi_point = Point {x: 20.0, y: 40.0};
-            let intro_dim: TextDimensions = draw_text("Hello, Macroquad!", 
-                hi_point.x, hi_point.y, 30.0, LIGHTGRAY);
-
-            draw_rectangle_lines(hi_point.x, hi_point.y-20.0, intro_dim.width, 
-                intro_dim.height+change, 4.0, BLACK);
-
-            // if change > 20.0 {
-            //     change = 0.0;
-            // } else {
-            //     change += 1.0;
-            // }
-            
-            let side = (Instant::now() - start_time).as_secs() % 2 == 1;
-
-            draw_texture(coin.flip_coin(start_time, Some(side)), screen_height() * 0.3, 
-                screen_width() * 0.3, WHITE);
-
-            let mouse = mouse_position();
-            draw_circle(mouse.0, mouse.1, 5.0, YELLOW);
-
-            if is_mouse_button_down(MouseButton::Left) {
-                draw_circle(mouse.0, mouse.1, 2.0, GREEN);
-            }
-            if is_mouse_button_down(MouseButton::Right) {
-                draw_circle(mouse.0, mouse.1, 2.0, BLUE);
-            }
-
-            next_frame().await
-        }
-    } else if is_help_cmd(mode) {
+    } else if _is_help_cmd(mode) {
         println!("The Improbability machine has a 2 modes it can run in.\n");
         println!("cmd: Command Line mode. Used for more direct debugging. Very basic.");
         println!("ui: The Game UI that will be used. Currently only barely functional, don't expect much.");
@@ -107,7 +69,7 @@ async fn main() {
     }
 }
 
-fn is_help_cmd(arg: &String) -> bool {
+fn _is_help_cmd(arg: &String) -> bool {
     arg == "help" ||
     arg == "Help" ||
     arg == "h" ||
